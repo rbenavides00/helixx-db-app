21bc6dc1850fe87e22f49948c779da295bfde4da
Roberto Benavides <roberto00bl@hotmail.com>
2022-10-19 11:53:42 -0500

    Heroku setup

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ const app = require('./app')
 
 const port = process.env.PORT || 3000
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
     console.log(`Servidor iniciado en el puerto ${port}`)
 })
